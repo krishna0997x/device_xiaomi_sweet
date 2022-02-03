144dc1059a6109f2ec283955cf4c11e2f4176518
jhenrique09 <jhsv09@gmail.com>
2022-02-03 16:54:46 +0530

    sweet: Enable quick tap

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -13,6 +13,8 @@ $(call inherit-product, device/xiaomi/sweet/device.mk)
 
 # Inherit some common PixelExtended stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
 
 PEX_BUILD_TYPE := UN-MINKAN
 
