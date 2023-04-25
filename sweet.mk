b85862126b0e02b3198b89d1a1797089c283ae11
pure-soul-kk <krishnakripa34567@gmail.com>
2023-04-25 21:58:59 +0530

    sweet: Project-Zephyrus Bringup

--- sweet.mk
+++ sweet.mk
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# OTA updater.
+CUSTOM_BUILD_TYPE := Official
 
-PRODUCT_NAME := lineage_sweet
+# Gapps build only.
+WITH_GMS := true
+
+PRODUCT_NAME := sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
